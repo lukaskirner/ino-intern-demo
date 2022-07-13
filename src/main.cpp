3728d0e8c7010ca97e0702e557e92e3affd5894a
Lukas Kirner <lkirner@inovex.de>
2022-07-13 17:14:08 +0200

    Added MQTT Connection via Wi-Fi

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,42 @@
 #include <Arduino.h>
+#include <ConnectionClient.h>
+#include "wifi_secrets.h"
+
+String wifiSSID = SECRET_SSID;
+String wifiPassword = SECRET_PASS;
+
+PubSubClient *client;
+
+void callback(char *topic, byte *payload, unsigned int length);
 
 void setup()
 {
   // Initialize serial.
   Serial.begin(9600);
 
+  // Establish connection to MQTT Broker by using built-in Wi-Fi.
+  ConnectionClient connectionClient;
+  connectionClient.connectToWiFi(wifiSSID.c_str(), wifiPassword.c_str());
+  client = connectionClient.initMQTTClient("broker.hivemq.com", 1883, "ino-intern-client");
+  client->setCallback(callback);
+
+  if (!client->subscribe("ino/intern/iot/led/#"))
+  {
+    Serial.println("MQTT subscription failed!");
+    delay(1000);
+  }
+  Serial.println("MQTT subscribe successfully");
+
   // put your setup code here, to run once:
 }
 
 void loop()
 {
+  client->loop();
   // put your main code here, to run repeatedly:
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.printf("Message arrived on topic %s\n", topic);
 }
